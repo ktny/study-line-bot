c447bf21178e3652ee80e2747c040c600d7c772a
ktny <katsunori.t10@gmail.com>
2023-02-08 06:40:50 +0900

    fix

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# line bot
-
-```sh
-# Dockerイメージのビルド
-docker build -t line-bot .
-
-# GCRへpush
-gcloud builds submit --tag gcr.io/[project-name]/line-bot --project [project-name]
-```
-
+# LINE BOT
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Dockerイメージのビルド
+# build Docker image
 docker build -t line-bot .
 
-# GCRへpush
+# push to GCR
 gcloud builds submit --tag gcr.io/tactical-curve-306812/line-bot --project tactical-curve-306812
+
+# deploy to CloudRun
+gcloud run deploy line-bot --image gcr.io/tactical-curve-306812/line-bot:latest --region us-central1 --project tactical-curve-306812
